698624984f2545c730c74ba807e3d571531bbb79
Yana Kormshchikova <kormshchikova_ye@magnit.ru>
2026-09-16 22:33:14 +0300

    добавила аллюр

--- tests/test_somth.py
+++ tests/test_somth.py
@@ -1,14 +1,16 @@
 import pytest
 from playwright.sync_api import sync_playwright
+import allure
 
+@allure.title('Open browser')
+@allure.description('click on btn 5 times')
 @pytest.mark.smoke
 def test_open_browser():
     pw = sync_playwright().start()
-
-    browser = pw.chromium.launch(headless=True)
-    page = browser.new_page()
-
-
+    with allure.step('launch browser'):
+        browser = pw.chromium.launch(headless=True)
+    with allure.step('open new page'):
+        page = browser.new_page()
 
     page.goto('https://the-internet.herokuapp.com/add_remove_elements/')
     page.locator('//button[text()="Add Element"]').click(click_count=5, delay=2000)
