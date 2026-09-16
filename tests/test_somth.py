211a9422f24bae34a74b98e738fe9ba8c4df6370
Yana Kormshchikova <kormshchikova_ye@magnit.ru>
2026-09-16 22:22:59 +0300

    настройка пайплайна

--- tests/test_somth.py
+++ tests/test_somth.py
@@ -8,16 +8,12 @@ def test_open_browser():
     browser = pw.chromium.launch(headless=True)
     page = browser.new_page()
 
-    # page.wait_for_timeout(3000)
 
 
     page.goto('https://the-internet.herokuapp.com/add_remove_elements/')
     page.locator('//button[text()="Add Element"]').click(click_count=5, delay=2000)
     page.locator('//button[text()="Add Element"]').click(force=True)
 
-    # page.locator('canvas').click(modifiers=['Shift'], position={'x':20, 'y': 32})
-
-    # page.wait_for_timeout(3000)
 
     browser.close()
     pw.stop()
